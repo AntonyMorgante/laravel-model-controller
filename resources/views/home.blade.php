2330f2c2d9733c3a9f5ee6810019ec6e395a81c1
AntonyMorgante <antonymorgante1@gmail.com>
2022-03-04 15:13:47 +0100

    basic css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,21 +4,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
-    <ul>
-        @foreach ($data as $movie)
-            <li>
-                <p>Titolo: {{$movie['title']}}</p>
-                <p>Titolo originale: {{$movie['original_title']}}</p>
-                <p>Anno: {{$movie['date']}}</p>
-                <p>Paese: {{$movie['nationality']}}</p>
-            </li>
-        @endforeach
-    </ul>
-
-
-
+    <header>
+        <h1>Film selezionati</h1>
+    </header>
+    <main>
+        <div class="container">
+            <ul>
+                @foreach ($data as $movie)
+                    <li>
+                        <div class="movie-info">
+                            <p><span class="bold-text">Titolo:</span> {{$movie['title']}}</p>
+                            <p><span class="bold-text">Titolo originale:</span> {{$movie['original_title']}}</p>
+                            <p><span class="bold-text">Anno:</span> {{$movie['date']}}</p>
+                            <p><span class="bold-text">Paese:</span> {{$movie['nationality']}}</p>
+                            <p><span class="bold-text">Voto:</span> {{$movie['vote']}}</p>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </main>
 </body>
 </html>
